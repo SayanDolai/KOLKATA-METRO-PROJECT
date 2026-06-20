54ab3291e95acb670366eb0004782f1d0678d770
SayanDolai <dolaisayan03@gmail.com>
2026-06-20 21:16:31 +0530

    Implemented fare calculator structor

--- Include/FareCalculator.h
+++ Include/FareCalculator.h
@@ -1,2 +1,32 @@
 // calculate fare on the basis of distance 
 // on the basis of distance 
+#ifndef FARECALCULATOR_H
+#define FARECALCULATOR_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class FareCalculator {
+
+private:
+
+    // Stores fare slabs
+    // {minimum_distance, maximum_distance, fare}
+    vector<pair<pair<double,double>,int>> fareTable;
+
+public:
+
+    // Load fare data from fares.txt
+    void loadFares(const string& filename);
+
+    // Return fare for a given distance
+    int calculateFare(double distance);
+
+    // Display all fare slabs for user friendly purpose and also maintain the transparency
+    void displayFareTable();
+};
+
+#endif
